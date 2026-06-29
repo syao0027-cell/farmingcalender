8829c94ea683f78c8e16c7393a3f62952dc9c41a
syao0027-cell <sya.o0027@gmail.com>
2026-06-29 12:13:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Netlifyの環境変数（隠し金庫）から安全に鍵を読み込む
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = https://tscogqlhlavulqgncodj.supabase.co/rest/v1/
+const supabaseKey = sb_publishable_baybqGP9O1REnkNCL7m3CA_V_8obPrS;
 
 // Supabaseクライアントの初期化
 const supabase = supabasejs.createClient(supabaseUrl, supabaseKey);
